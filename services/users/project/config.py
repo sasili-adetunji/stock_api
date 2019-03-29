f27fd0fcfae39fae4c2d348256fb07ac9d5d7f6c
Sasiliyu Adetunji <sasil.adetunji@gmail.com>
2019-03-29 18:12:26 +0100

    - adds test setup

--- services/users/project/config.py
+++ services/users/project/config.py
@@ -6,6 +6,7 @@ class BaseConfig:
     """Base configuration"""
     TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.environ.get('SECRET_KEY')
 
 class DevelopmentConfig(BaseConfig):
     """Development configuration"""
